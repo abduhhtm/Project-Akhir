ce44b5ada50abf5e7a094ecc34cec01fa5618822
abduhhtm <160695516+abduhhtm@users.noreply.github.com>
2025-02-26 13:47:02 +0700

    65gyt

--- abduh-projekterakhirreact/src/main.jsx
+++ abduh-projekterakhirreact/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-
+import { Shop } from './App.jsx'
+import { LoginPage } from './loginPage.jsx'
+import { ProductList } from './ProductList.jsx'
+import Axioshop18 from './meeting/project/path.jsx'
+import Project from "./projek/praktek/path.jsx";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    < Project />
   </StrictMode>,
 )
