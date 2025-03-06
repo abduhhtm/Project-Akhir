844c33f0226882be885b2fc74f4b3129730f7d86
abduhhtm <160695516+abduhhtm@users.noreply.github.com>
2025-03-06 11:48:38 +0700

    nvrhv

--- abduh-projekterakhirreact/src/main.jsx
+++ abduh-projekterakhirreact/src/main.jsx
@@ -5,7 +5,7 @@ import { Shop } from './App.jsx'
 import { LoginPage } from './loginPage.jsx'
 import { ProductList } from './ProductList.jsx'
 import Axioshop18 from './meeting/project/path.jsx'
-import Project from "./projek/praktek/path.jsx";
+import {Project} from "./projek/praktek/path.jsx";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     < Project />
